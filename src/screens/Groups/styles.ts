6c83984df63b0283690fd16edd0baa497f5be781
RodrigoTRS <rodrigoteix1998@gmail.com>
2024-03-26 08:42:50 -0300

    Adding styled components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native"
+import { Text, View } from 'react-native';
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #000;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Title = styled.Text`
+    color: #fff;
+    font-size: 32px;
+`
